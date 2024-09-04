09b8fbc57b612fd7eff5127ad00ebe405cc014a4
Maximiliano Zogbi <git@clonix.com.ar>
2024-09-04 02:22:15 -0300

    Version

--- src/Exceptions/CouldNotSendNotification.php
+++ src/Exceptions/CouldNotSendNotification.php
@@ -24,11 +24,10 @@ class CouldNotSendNotification extends Exception
         $statusCode = $exception->getResponse()->getStatusCode();
 
         $result = json_decode($exception->getResponse()->getBody()->getContents(), false);
-        $description = $result->description ?? '';
-        $message = $result->message ?? '';
-        $error = $result->error ?? '';
+        $description = $result->description ?? $result->message ??  $result->error ?? 'no description given';
+ 
 
-        return new static("Whatsapp Api Response Error:  `{$statusCode} {$description} {$message} {$error}`", 0, $exception);
+        return new static("Whatsapp Api Response Error:  `{$statusCode} - {$description}`", 0, $exception);
     }
 
     /**
